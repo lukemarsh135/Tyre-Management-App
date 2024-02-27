cf94cdfe0eb81c4634c7c8f8c966690d8bf62f03
Luke <Luke.Marshall@kerridgecs.com>
2024-02-27 14:22:00 +0000

    Added template for work orders for command pattern

---
+++ TyreManagementAppOOP/Models/GenerateWorkOrderDocument.cs
@@ -0,0 +1,20 @@
+﻿using TyreManagementAppOOP.Commands;
+using TyreManagementAppOOP.Interfaces;
+
+namespace TyreManagementAppOOP.Models
+{
+    public class GenerateWorkOrderDocument : WorkOrderCommand
+    {
+        private readonly string _filePath;
+
+        public GenerateWorkOrderDocument(Sale sale, string filePath) : base(sale)
+        {
+            _filePath = filePath;
+        }
+
+        public override void Execute()
+        {
+            // Logic to format and write work order data to a file
+        }
+    }
+}
